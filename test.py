4a111287211f9eb9a02046642542acd596950c4f
arunavs94 <asingh94@ucla.edu>
2017-04-17 01:09:16 -0700

    Added part 2d

---
+++ test.py
@@ -0,0 +1,19 @@
+import random as rnd
+
+def main():
+
+	print rnd.randrange(0,100)
+
+	print rnd.randrange(0,100)
+	
+	print rnd.randrange(0,100)
+	print rnd.randrange(0,100)
+	print rnd.randrange(0,100)
+	print rnd.randrange(0,100)
+	print rnd.randrange(0,100)
+	print rnd.randrange(0,100)
+	print rnd.randrange(0,100)
+	print rnd.randrange(0,100)
+
+if __name__ == '__main__':
+	main()
